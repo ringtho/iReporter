1ad28b6d76d7d10c909a5380ce57a9ff3b8f973a
Smith Ringtho <sringtho@gmail.com>
2019-01-20 13:43:28 +0300

    add main.js containing javascript for delete button in list.html

---
+++ UI/script/main.js
@@ -0,0 +1,27 @@
+ $(document).ready(function(){
+        $("#btn2").click(function(){
+        confirm("Are you sure you want to delete this record?!");
+        $(".grid1").remove();
+
+          });
+        });
+
+$(document).ready(function(){
+    $("#btn23").click(function(){
+        confirm("Are you sure you want to delete this record?!");
+        $(".grid2").remove();
+           });
+        });
+$(document).ready(function(){          
+        $("#btn24").click(function(){
+        confirm("Are you sure you want to delete this record?!");
+        $(".grid3").remove();
+               });
+        });
+ 
+$(document).ready(function(){
+        $("#btn25").click(function(){
+        confirm("Are you sure you want to delete this record?!");
+        $(".grid4").remove();
+               });
+        });
